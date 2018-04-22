8d395afd3fed5ff563aa970c53bbeebfa88cb503
Kingsley <kingsley.xym@gmail.com>
2018-04-23 00:27:18 +0800

    Add Maze Setup Function

--- maze.js
+++ maze.js
@@ -1,4 +1,48 @@
-const generatex = 15, generatey = 15;
+const
+	MAX_RANGE_OF_MAZE = 60,
+	MIN_RANGE_OF_MAZE = 3,
+	generatex = 12,
+	generatey = 15,
+	outsetx = 2,
+	outsety = 1,
+	terminalx = 2 * generatex,
+	terminaly = 2 * generatey + 1,
+	mazerangex = 2 * generatex + 1,
+	mazerangey = 2 * generatey + 1;
+
+var maze = new Array(MAX_RANGE_OF_MAZE);
+for (var i = 0; i < MAX_RANGE_OF_MAZE; i++)
+	maze[i] = new Array(MAX_RANGE_OF_MAZE);
+
+function setup() {
+	for (var i = 0; i <= generatex * 2 + 2; i++)
+		for (var j = 0; j <= generatey * 2 + 2; j++)
+			maze[i][j] = (i + j - 2) % 14 + 1;
+
+	for (var i = 0; i <= 2 * generatey + 2; i++ ) {
+		maze[0][i] = 0; maze[2 * generatex + 2][i] = 0;
+	}
+
+	for (var i = 0; i <= 2 * generatex + 2; i++ ) {
+		maze[i][0] = 0; maze[i][2 * generatey + 2] = 0;
+	}
+
+	maze[outsetx][outsety] = 15; maze[terminalx][terminaly] = 0;
+}
+setup();
+console.table(maze);
+
+generateMaze(
+	Math.ceil(Math.random() * generatex),
+	Math.ceil(Math.random() * generatey)
+);
+
+function generateMaze(x, y) {
+}
+
+
+
+
 
 var canvas = document.getElementById('canvas');
 canvas.width = '900';
@@ -19,16 +63,9 @@ for (var i = 0; i < generatex; i++) {
 	}
 }
 
+
 resetColor(0, 0, 'RED');
 resetColor(3, 5, 'BLUE');
-
-generateMaze(
-	Math.ceil(Math.random() * generatex),
-	Math.ceil(Math.random() * generatey)
-);
-
-function generateMaze(x, y) {
-}
 
 function resetColor(x, y, color) {
 	ctx.fillStyle = color;
