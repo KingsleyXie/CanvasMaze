eaf3aa916eb9384da156587edb5592d4d543baa7
Kingsley <kingsley.xym@gmail.com>
2018-04-27 01:52:52 +0800

    :tada: :bookmark: :art: Finish Initial CanvasMaze

--- maze.js
+++ maze.js
@@ -1,10 +1,8 @@
 const
-	MAX_RANGE_OF_MAZE = 60,
+	MAX_RANGE_OF_MAZE = 50,
 	MIN_RANGE_OF_MAZE = 3,
-	generatex = 13,
-	generatey = 9,
-	outsetx = 2,
-	outsety = 1,
+	generatex = 23, generatey = 11,
+	outsetx = 2, outsety = 1,
 	terminalx = 2 * generatex,
 	terminaly = 2 * generatey + 1,
 	mazerangex = 2 * generatex + 1,
@@ -15,37 +13,25 @@ const dx = [0, 1, 0, -1], dy = [1, 0, -1, 0];
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-canvas.width = '900'; canvas.height = '900';
-canvas.style.width = '900px'; canvas.style.height = '900px';
-
 var maze = new Array(MAX_RANGE_OF_MAZE);
 for (var i = 0; i < MAX_RANGE_OF_MAZE; i++)
 	maze[i] = new Array(MAX_RANGE_OF_MAZE);
 
-var Q = new Array(), Qupdated;
-
-var QnextNum = new Array(MAX_RANGE_OF_MAZE);
-for (var i = 0; i < MAX_RANGE_OF_MAZE; i++) {
-	QnextNum[i] = new Array(MAX_RANGE_OF_MAZE);
-	QnextNum[i].fill(0);
-}
-
-var Qpre = new Array(MAX_RANGE_OF_MAZE);
-for (var i = 0; i < MAX_RANGE_OF_MAZE; i++) {
-	Qpre[i] = new Array(MAX_RANGE_OF_MAZE);
-	Qpre[i].fill([0, 0]);
-}
-
 let maze_dumps = [];
-let searching = true;
+let dfs_searching = true;
 
-setup();
+init();
 generateMaze(randint(generatex), randint(generatey));
-searchMazeWithDFS(outsetx, outsety);
+// searchMazeWithDFS(outsetx, outsety);
+searchMazeWithBFS(outsetx, outsety);
 
+function init() {
+	canvas.width = (mazerangex + 2) * 25;
+	canvas.height = (mazerangey + 2) * 25;
 
+	canvas.style.width = (mazerangex + 2) * 25;
+	canvas.style.height = (mazerangey + 2) * 25;
 
-function setup() {
 	for (var i = 0; i <= generatex * 2 + 2; i++)
 		for (var j = 0; j <= generatey * 2 + 2; j++)
 			maze[i][j] = (i + j - 2) % 14 + 1;
@@ -58,7 +44,8 @@ function setup() {
 		maze[i][0] = 0; maze[i][2 * generatey + 2] = 0;
 	}
 
-	maze[outsetx][outsety] = 15; maze[terminalx][terminaly] = 0;
+	maze[outsetx][outsety] = 15;
+	maze[terminalx][terminaly] = 0;
 }
 
 function generateMaze(x, y) {
@@ -71,11 +58,8 @@ function generateMaze(x, y) {
 		i < 4; i++, step = (step + phase) % 4) {
 		if (doubley + dy[step] - 1 != mazerangey
 			&& maze[doublex + 2 * dx[step]][doubley + 2 * dy[step]]) {
-			maze[doublex + dx[step]][doubley + dy[step]] = -1;
-
-			maze_dumps.push(JSON.stringify(maze));
-
 			maze[doublex + dx[step]][doubley + dy[step]] = 0;
+			maze_dumps.push(JSON.stringify(maze));
 			generateMaze(x + dx[step], y + dy[step]);
 		}
 	}
@@ -84,23 +68,25 @@ function generateMaze(x, y) {
 function searchMazeWithDFS(x, y) {
 	if (x == terminalx && y == terminaly) {
 		showProcess();
-		searching = false;
+		dfs_searching = false;
 		throw "Terminal reached in DFS.";
 	} else {
-		for (let step = 0; step < 4; step++) {
+		for (var step = 0; step < 4; step++) {
 			if ((x + dx[step] > 0)
 				&& (x + dx[step] <= mazerangex)
 				&& (y + dy[step] > 0)
 				&& (y + dy[step] <= mazerangey)
 				&& !maze[x + dx[step]][y + dy[step]]) {
-				x = x + dx[step]; y = y + dy[step]; maze[x][y] = 15;
+				x = x + dx[step]; y = y + dy[step];
 
+				maze[x][y] = 15;
 				maze_dumps.push(JSON.stringify(maze));
+
 				searchMazeWithDFS(x, y);
-				maze_dumps.push(JSON.stringify(maze));
 
-				if (searching) {
+				if (dfs_searching) {
 					maze[x][y] = 16; x = x - dx[step]; y = y - dy[step];
+					maze_dumps.push(JSON.stringify(maze));
 				}
 			}
 		}
@@ -108,50 +94,67 @@ function searchMazeWithDFS(x, y) {
 }
 
 function searchMazeWithBFS(x, y) {
+	var Q = new Array(), Qupdated;
+
+	var QnextNum = new Array(MAX_RANGE_OF_MAZE);
+	for (var i = 0; i < MAX_RANGE_OF_MAZE; i++) {
+		QnextNum[i] = new Array(MAX_RANGE_OF_MAZE);
+		QnextNum[i].fill(0);
+	}
+
+	var Qpre = new Array(MAX_RANGE_OF_MAZE);
+	for (var i = 0; i < MAX_RANGE_OF_MAZE; i++) {
+		Qpre[i] = new Array(MAX_RANGE_OF_MAZE);
+		Qpre[i].fill([0, 0]);
+	}
+
 	Q.push([x, y]);
 	while (Q.length > 0) {
-		x = Q[Q.length - 1][0]; y = Q[Q.length - 1][1]; Qupdated = 0;
-		console.log(Q[0]);
-		console.log(x, y);
+		x = Q[0][0]; y = Q[0][1]; Qupdated = 0;
 		for (var step = 0; step < 4; step++) {
 			if ((x + dx[step] > 0)
 				&& (x + dx[step] <= mazerangex)
 				&& (y + dy[step] > 0)
 				&& (y + dy[step] <= mazerangey)
 				&& !maze[x + dx[step]][y + dy[step]]) {
+
 				var xstep = x + dx[step], ystep = y + dy[step];
 				maze[xstep][ystep] = 17;
-				Q.push([xstep, ystep]); Qupdated = 1;
-				console.log('Push', xstep, ystep);
-				console.log(Q[0]);
-				console.log(Q[1]);
+				maze_dumps.push(JSON.stringify(maze));
 
-				Qpre[xstep][ystep] = [x, y]; QnextNum[x][y]++;
+				Q.push([xstep, ystep]); Qupdated = 1;
+				Qpre[xstep][ystep] = [x, y];
+				QnextNum[x][y]++;
 
 				if (xstep == terminalx && ystep == terminaly) {
 					do {
 						maze[xstep][ystep] = 15;
+						maze_dumps.push(JSON.stringify(maze));
+
 						var xtemp = Qpre[xstep][ystep][0];
 						ystep = Qpre[xstep][ystep][1];
 						xstep = xtemp;
 					} while (xstep && ystep);
-					console.log("已到达迷宫的出口位置\n");
-					throw new Error("Terminal Reached!");
+
+					showProcess();
+					throw "Terminal reached in BFS.";
 				}
 			}
 		}
 
 		if (!Qupdated) {
-			var deletex = x, deletey = y;
-			do {
-				maze[deletex][deletey] = 16;
+			var popx = x, popy = y;
+			while (popx && popy
+				&& !QnextNum[popx][popy]) {
+				maze[popx][popy] = 16;
+				maze_dumps.push(JSON.stringify(maze));
 
-				var xtemp = Qpre[deletex][deletey][0];
-				deletey = Qpre[deletex][deletey][1];
-				deletex = xtemp;
+				var xtemp = Qpre[popx][popy][0];
+				popy = Qpre[popx][popy][1];
+				popx = xtemp;
 
-				QnextNum[deletex][deletey]--;
-			} while (deletex && deletey && !QnextNum[deletex][deletey]);
+				QnextNum[popx][popy]--;
+			}
 		}
 		Q.shift();
 	}
@@ -161,22 +164,18 @@ function randint(max) {
 	return Math.ceil(Math.random() * max);
 }
 
-function paintMaze(m, presentx, presenty) {
+function paintMaze(m) {
 	var color;
 	for (var i = 1; i <= mazerangex; i++) {
 		for (var j = 1; j <= mazerangey; j++) {
-			if ((i == presentx && j == presenty)
-				|| (m[i][j] == -1)) {
-				color = '#AEB6BF';
-			} else {
-				switch (m[i][j]) {
-					case 0: color = '#CCD1D1'; break;
-					case 15: color = '#3498DB'; break;
-					case 16: color = '#E74C3C'; break;
-					case 17: color = '#E74C3C'; break;
-					default: color = '#7F8C8D'; break;
-				}
+			switch (m[i][j]) {
+				case 0: color = '#CCD1D1'; break; //Accessable Paths
+				case 15: color = '#48C9B0'; break; // Maze Solution
+				case 16: color = '#F1948A'; break; // Unaccessable Paths
+				case 17: color = '#85C1E9'; break; // Trying Paths In BFS
+				default: color = '#99A3A4'; break; // Walls
 			}
+
 			ctx.fillStyle = color;
 			ctx.fillRect(i * 25, j * 25, 25, 25);
 		}
@@ -187,7 +186,7 @@ function showProcess() {
 	maze_dumps.forEach(function(m, i) {
 		setTimeout(function() {
 			paintMaze(JSON.parse(m));
-		}, 50 * i);
+		}, 30 * i);
 	});
 	paintMaze(maze);
 }
